8e014744ed53e061a84ae808d1aecfcfffddaabd
inggabo001 <gabojunior05@gmail.com>
2018-05-18 16:44:21 -0400

    prueba de geolocalizacion con mapa

--- www/js/index.js
+++ www/js/index.js
@@ -32,15 +32,22 @@ var app = {
 
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+        navigator.geolocation.getCurrentPosition(onMapSuccess, onMapError, { enableHighAccuracy: true });
     }
 };
 
 app.initialize();
+
+var onMapSuccess = function (position) {
+
+    Latitude = position.coords.latitude;
+    Longitude = position.coords.longitude;
+ 
+    crear_mapa();
+
+}
+
+function onMapError(error) {
+    console.log('code: ' + error.code + '\n' +
+        'message: ' + error.message + '\n');
+}
